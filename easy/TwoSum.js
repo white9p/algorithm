dde641c6b4e1043c4ae838d3a3a122bfbfd412d1
white9p <kmjkmj9905@naver.com>
2021-06-19 11:40:49 +0900

    first commit

---
+++ easy/TwoSum.js
@@ -0,0 +1,22 @@
+// Input: nums = [11,2,15,7], target = 9
+//  Output: [0,1]
+//  Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+  let n = nums.length;
+  for (i = 0; i < n; i++) {
+    if (nums[i] <= target) {
+      for (j = i + 1; j < n - 1; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+};
